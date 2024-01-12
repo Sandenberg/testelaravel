7ab14c01808df7215c8f47b7160cd171eb580d23
sandenberg <sandenberg@gmail.com>
2024-01-12 03:19:00 -0300

    Fix

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\ContactsImport;
-use App\Models\Contact;
 use App\Models\ScheduledNotification;
 use Carbon\Carbon;
 
